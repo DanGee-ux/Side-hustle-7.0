47e45d037147566404c6f37855a9f3216c08a943
Igweze Daniel <igweze.dan15@gmail.com>
2023-03-10 15:57:55 +0100

    first commit

--- datatypes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$name = "Tunde";
-$occupation = "Bricklayer";
-
-echo $name." is a ".$occupation;
-?>
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-   define("message", "sup");
-   echo message;
-  
+
+$name = "<b><b><b><i>Daniel</i></b></b></b>";
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Side Hustle 7.0 backend project</title>
+</head>
+<body style = "background-color: #C69EB8">
+    <p style = "text-align: center; 
+                font-size: 500%; 
+                background-color: #F49CBB; 
+                color: #EEEEFF; 
+                box-shadow: 20px 20px 40px -6px rgba(0,0,0,0.2);
+                border-radius: 10px;
+"><?php echo "<b><b><b><i>My name is</i></b></b></b>".' '.$name;?></p>
+</body>
+</html>
